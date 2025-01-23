69f6b14fb4e38c13ffdf28f0414652a31f97e34d
bear148 <bizzeebear@hotmail.com>
2025-01-22 22:52:15 -0800

    Added credit counter

--- scripts/index.js
+++ scripts/index.js
@@ -7,8 +7,11 @@ going to expand upon it in my own tank themed game.
 
 - Michael S. Jan 22. 2024
 */
-
+let countElement = document.getElementById("count");
 let gold = 0;
+let credits = 0;
+let gold_rate = 1;
+let credit_rate = 100;
 
 async function init() {
     counter();
@@ -22,13 +25,34 @@ async function counter() {
     while(true) {
         await sleep(1000)
         console.log("1+ Gold");
-        gold++;
+        console.log("+100 Credits");
+        gold += gold_rate;
+        credits += credit_rate;
         update_counters();
     }
 }
 
+function fadeOut(element, duration) {
+    let opacity = 1;
+    let intervalId = setInterval(function() {
+        if (opacity > 0) {
+            opacity -= 0.01;
+            element.style.opacity = opacity;
+        } else {
+            clearInterval(intervalId);
+        }
+    }, duration / 100);
+}
+
 function update_counters() {
     let gold_t = document.getElementById("gold_c");
+    let credit_t = document.getElementById("credit_c");
+
+    countElement.style.display = "unset";
+    countElement.innerHTML = "+" + gold_rate;
 
     gold_t.innerHTML = gold;
+    credit_t.innerHTML = credits;
+    
+    fadeOut(countElement, 100);
 }
