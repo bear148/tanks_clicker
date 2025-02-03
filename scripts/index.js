3d58ec9cd4e3c8cfe48a25f9d6a7415fc264bf06
bear148 <bizzeebear@hotmail.com>
2025-02-02 20:57:30 -0800

    Base game, added purchasable items

--- scripts/index.js
+++ scripts/index.js
@@ -10,49 +10,125 @@ going to expand upon it in my own tank themed game.
 let countElement = document.getElementById("count");
 let gold = 0;
 let credits = 0;
+let creditsDisplay = 0;
 let gold_rate = 1;
-let credit_rate = 100;
+let credit_rate = 10;
+let T = 0;
 
-async function init() {
-    counter();
+let gps = 0;
+
+let popUps = [];
+let popUpsEnabled = true;
+
+let items = [];
+
+let crew = 0;
+
+let Load = function () {
+    Main();
 }
 
-function sleep(ms) {
-    return new Promise(resolve => setTimeout(resolve, ms || DEF_DELAY));
-}
+let item = function (name, description, img, pirce, func) {
+    this.name = name;
+    this.desc = description;
+    this.img = img;
+    this.price = pirce;
+    this.func = func;
+    items[name] = this;
 
-async function counter() {
-    while(true) {
-        await sleep(1000)
-        console.log("1+ Gold");
-        console.log("+100 Credits");
-        gold += gold_rate;
-        credits += credit_rate;
-        update_counters();
+    this.Buy = function () {
+        if (credits >= this.price) {
+            credits -= this.price;
+            this.price = Math.ceil(this.price * 1.1);
+            this.func(1);
+        }
     }
 }
 
-function fadeOut(element, duration) {
-    let opacity = 1;
-    let intervalId = setInterval(function() {
-        if (opacity > 0) {
-            opacity -= 0.01;
-            element.style.opacity = opacity;
-        } else {
-            clearInterval(intervalId);
-        }
-    }, duration / 100);
+let Buy = function (i) {
+    items[i].Buy();
 }
 
-function update_counters() {
-    let gold_t = document.getElementById("gold_c");
-    let credit_t = document.getElementById("credit_c");
+new item("Crew", "A crew member to help skillfully guide your tank.", null, 2000, function (buy) {
+    if (buy) crew++;
+    let str = '';
+    for (let i = 0; i < crew; i++) {
+        let x = Math.floor(Math.random() * 20 + (i % 10) * 24);
+        let y = Math.floor(Math.random() * 20 + Math.floor(i / 10) * 24);
+        str += '<div class="crew" style="left:' + x + 'px;top:' + y + 'px;"></div>';
+    }
+    l('crew-members').innerHTML = str;
+})
 
-    countElement.style.display = "unset";
-    countElement.innerHTML = "+" + gold_rate;
+let PopUp = function (el, str) {
+    this.el = el;
+    this.str = str;
+    this.life = 0;
+    this.offx = Math.floor(Math.random() * 20 - 10);
+    this.offy = Math.floor(Math.random() * 20 - 10);
+    popUps.push(this);
+};
 
-    gold_t.innerHTML = gold;
-    credit_t.innerHTML = credits;
-    
-    fadeOut(countElement, 100);
+function l(str) {
+    return document.getElementById(str);
+}
+
+function tankClick() {
+    credits += credit_rate;
+    if (popUps.length < 260 && popUpsEnabled) new PopUp("tank", "+" + credit_rate);
+    console.log(credits);
+}
+
+function addCredits(howmany, el) {
+    credits += howmany;
+    if (el && popUps.length < 250 && popUpsEnabled) new PopUp(el, '+' + howmany);
+}
+
+let Main = function () {
+    let str = "";
+
+    for (let i in popUps) {
+        let rect = l(popUps[i].el).getBoundingClientRect();
+        let x = Math.floor((rect.left + rect.right) / 2 + popUps[i].offx) - 20;
+        let y =
+            Math.floor(
+                (rect.top + rect.bottom) / 2 -
+                Math.pow(popUps[i].life / 100, 0.5) * 100 +
+                popUps[i].offy
+            ) - 30;
+        let opacity = 1 - (Math.max(popUps[i].life, 80) - 80) / 20;
+        str +=
+            '<div class="pop" style="position:absolute;left:' +
+            x +
+            "px;top:" +
+            y +
+            "px;opacity:" +
+            opacity +
+            ';">' +
+            popUps[i].str +
+            "</div>";
+        popUps[i].life += 2;
+        if (popUps[i].life >= 100) popUps.splice(i, 1);
+    }
+    l("popups").innerHTML = str;
+
+    for (let i in items) {
+        if (credits >= items[i].price) l('buy' + items[i].name).className = '';
+        else l('buy' + items[i].name).className = 'na';
+    }
+
+    if (crew && T % Math.ceil(150 / crew) == 0) addCredits(50, 'crew-members');
+
+    let cps = 0;
+    cps += crew * 10 / 5;
+
+    // let floater = Math.round(cps * 10 - Math.floor(cps) * 10);
+    // cps = (cps) + (floater ? ('.' + floater) : '');
+    // l('credit_rate').innerHTML = 'credits/second : ' + cps;
+
+    creditsDisplay += (credits - creditsDisplay) * 0.5;
+    l('credit_c').innerHTML = Math.round(creditsDisplay);
+
+    T++;
+    setTimeout(Main, 1000 / 30);
 }
